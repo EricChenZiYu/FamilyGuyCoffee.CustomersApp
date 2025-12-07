3a59cab40badadd6d8f2e1f4a58b82e8c3a589dd
EricChenZiYu <ziyucrc@gmail.com>
2025-12-07 08:53:05 +0800

    更新主窗口布局和事件处理逻辑
    在 `MainWindow.xaml` 中，调整了菜单和网格的列跨度，增加了新的行定义，并添加了输入控件以收集用户信息。删除了一个按钮并添加了新的按钮，绑定了 `ButtonBase_OnClick` 事件。
    
    在 `MainWindow.xaml.cs` 中，移除了不必要的 `using` 语句，并实现了 `ButtonBase_OnClick` 方法以处理按钮点击事件，更新 `CustomerListGrid` 的列位置。

--- FamilyGuyCoffee.CustomersApp/MainWindow.xaml.cs
+++ FamilyGuyCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FamilyGuyCoffee.CustomersApp
 {
@@ -23,6 +15,19 @@ namespace FamilyGuyCoffee.CustomersApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newColumn);
+
 
         }
     }
