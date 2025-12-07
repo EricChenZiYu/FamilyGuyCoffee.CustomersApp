805c0b48ab06046bef984856fa2cf8fab3b69273
EricChenZiYu <ziyucrc@gmail.com>
2025-12-07 08:16:18 +0800

    添加项目文件。

---
+++ FamilyGuyCoffee.CustomersApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FamilyGuyCoffee.CustomersApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
