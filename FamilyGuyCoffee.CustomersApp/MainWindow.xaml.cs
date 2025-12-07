a9a3b3b760f393b6a3c5b57280591045b70de447
EricChenZiYu <ziyucrc@gmail.com>
2025-12-07 16:00:27 +0800

    添加 HeaderControl 和 CustomersView 用户控件
    在 `HeaderControl.xaml` 中添加了新的用户控件，显示应用程序名称和版本信息，并在 `MainWindow.xaml` 中集成。移除了原有的栅格布局，简化了界面结构。新增 `CustomersView` 用户控件，包含客户列表和输入框，增强了用户界面的可用性和清晰度。同时更新了相关的代码文件以支持这些更改。

--- FamilyGuyCoffee.CustomersApp/MainWindow.xaml.cs
+++ FamilyGuyCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace FamilyGuyCoffee.CustomersApp
 {
@@ -18,17 +17,6 @@ namespace FamilyGuyCoffee.CustomersApp
 
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
-            //var newColumn = column == 0 ? 2 : 0;
-            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
 
-            var column = Grid.GetColumn(CustomerListGrid);
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(CustomerListGrid, newColumn);
-
-
-        }
     }
 }
---
+++ FamilyGuyCoffee.CustomersApp/Views/CustomersView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FamilyGuyCoffee.CustomersApp.Views
+{
+    /// <summary>
+    /// CustomersView.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newColumn);
+
+
+        }
+    }
+}
